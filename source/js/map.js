9f523401854d6e328ac156cc7a9dba5e664c79d3
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-03-13 00:32:39 +0300

    style: footer

--- source/js/map.js
+++ source/js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
   const LOCATION = { lat: 59.938891, lng: 30.323037 };
 
-  const map = new google.maps.Map(document.querySelector(`#map`), {
+  const map = new google.maps.Map(document.querySelector('#map'), {
     zoom: 14,
     center: LOCATION,
     disableDefaultUI: true,
