7e9fa4e0ca368c02979d1b778f090ca949786aa1
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-03-19 19:08:52 +0300

    refactor: remove chaining operators for linter

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -2,27 +2,35 @@
 
 (() => {
   const mainNavElement = document.querySelector('.main-nav');
-  const mainNavLogo = mainNavElement?.querySelector('.main-nav__logo');
-  const mainNavToggle = mainNavElement?.querySelector('.main-nav__toggle');
-  const mainNavSiteNav = mainNavElement?.querySelector('.main-nav__site-nav');
 
-  const elements = {
-    'main-nav': mainNavElement,
-    'main-nav__logo': mainNavLogo,
-    'main-nav__toggle': mainNavToggle,
-    'main-nav__site-nav': mainNavSiteNav,
-  };
+  if (mainNavElement) {
+    const mainNavLogo = mainNavElement.querySelector('.main-nav__logo');
+    const mainNavToggle = mainNavElement.querySelector('.main-nav__toggle');
+    const mainNavSiteNav = mainNavElement.querySelector('.main-nav__site-nav');
 
-  Object.keys(elements).forEach((element) => {
-    elements[element]?.classList.remove(`${element}--no-js`);
-    elements[element]?.classList.add(`${element}--closed`);
-  })
+    const elements = {
+      'main-nav': mainNavElement,
+      'main-nav__logo': mainNavLogo,
+      'main-nav__toggle': mainNavToggle,
+      'main-nav__site-nav': mainNavSiteNav,
+    };
 
-
-  mainNavToggle?.addEventListener('click', () => {
     Object.keys(elements).forEach((element) => {
-      elements[element]?.classList.toggle(`${element}--opened`);
-      elements[element]?.classList.toggle(`${element}--closed`);
+      if (elements[element] !== undefined) {
+        elements[element].classList.remove(`${element}--no-js`);
+        elements[element].classList.add(`${element}--closed`);
+      }
     })
-  });
+
+    if (mainNavToggle) {
+      mainNavToggle.addEventListener('click', () => {
+        Object.keys(elements).forEach((element) => {
+          if (elements[element] !== undefined) {
+            elements[element].classList.toggle(`${element}--opened`);
+            elements[element].classList.toggle(`${element}--closed`);
+          }
+        })
+      });
+    }
+  }
 })();
