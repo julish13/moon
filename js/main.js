ebdeab9b39a872047ecd8b56e667dd64b546997f
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-03-19 19:10:31 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,40 +1,38 @@
-function initMap() {
-  const LOCATION = { lat: 59.938891, lng: 30.323037 };
-
-  const map = new google.maps.Map(document.querySelector('#map'), {
-    zoom: 14,
-    center: LOCATION,
-    disableDefaultUI: true,
-  });
-}
-
 'use strict';
 
 (() => {
   const mainNavElement = document.querySelector('.main-nav');
-  const mainNavLogo = mainNavElement?.querySelector('.main-nav__logo');
-  const mainNavToggle = mainNavElement?.querySelector('.main-nav__toggle');
-  const mainNavSiteNav = mainNavElement?.querySelector('.main-nav__site-nav');
 
-  const elements = {
-    'main-nav': mainNavElement,
-    'main-nav__logo': mainNavLogo,
-    'main-nav__toggle': mainNavToggle,
-    'main-nav__site-nav': mainNavSiteNav,
-  };
+  if (mainNavElement) {
+    const mainNavLogo = mainNavElement.querySelector('.main-nav__logo');
+    const mainNavToggle = mainNavElement.querySelector('.main-nav__toggle');
+    const mainNavSiteNav = mainNavElement.querySelector('.main-nav__site-nav');
 
-  Object.keys(elements).forEach((element) => {
-    elements[element]?.classList.remove(`${element}--no-js`);
-    elements[element]?.classList.add(`${element}--closed`);
-  })
+    const elements = {
+      'main-nav': mainNavElement,
+      'main-nav__logo': mainNavLogo,
+      'main-nav__toggle': mainNavToggle,
+      'main-nav__site-nav': mainNavSiteNav,
+    };
 
-
-  mainNavToggle?.addEventListener('click', () => {
     Object.keys(elements).forEach((element) => {
-      elements[element]?.classList.toggle(`${element}--opened`);
-      elements[element]?.classList.toggle(`${element}--closed`);
+      if (elements[element] !== undefined) {
+        elements[element].classList.remove(`${element}--no-js`);
+        elements[element].classList.add(`${element}--closed`);
+      }
     })
-  });
+
+    if (mainNavToggle) {
+      mainNavToggle.addEventListener('click', () => {
+        Object.keys(elements).forEach((element) => {
+          if (elements[element] !== undefined) {
+            elements[element].classList.toggle(`${element}--opened`);
+            elements[element].classList.toggle(`${element}--closed`);
+          }
+        })
+      });
+    }
+  }
 })();
 
 'use strict';
