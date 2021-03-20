b88e28c4429af0a374d56f6a605de1cc85cbbc84
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-03-20 22:16:37 +0300

    chore: add build

---
+++ build/js/main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+(() => {
+  const mainNavElement = document.querySelector('.main-nav');
+  const mainElement = document.querySelector('.main');
+  const footerElement = document.querySelector('.page-footer');
+
+  if (mainNavElement) {
+    const mainNavLogoElement = mainNavElement.querySelector('.main-nav__logo');
+    const mainNavToggleElement = mainNavElement.querySelector('.main-nav__toggle');
+    const mainNavSiteNavElement = mainNavElement.querySelector('.main-nav__site-nav');
+    const navItemsCollection = mainNavElement.querySelectorAll('.site-nav__item');
+
+    const elements = {
+      'main-nav': mainNavElement,
+      'main-nav__logo': mainNavLogoElement,
+      'main-nav__toggle': mainNavToggleElement,
+      'main-nav__site-nav': mainNavSiteNavElement,
+    };
+
+    Object.keys(elements).forEach((element) => {
+      if (elements[element] !== undefined) {
+        elements[element].classList.remove(`${element}--no-js`);
+        elements[element].classList.add(`${element}--closed`);
+      }
+    })
+
+    const toggleMenu = () => {
+      Object.keys(elements).forEach((element) => {
+        if (elements[element] !== undefined) {
+          elements[element].classList.toggle(`${element}--opened`);
+          elements[element].classList.toggle(`${element}--closed`);
+        }
+      })
+      if (mainElement) {
+        mainElement.classList.toggle('main--menu-open');
+      }
+      if (footerElement) {
+        footerElement.classList.toggle('page-footer--menu-open');
+      }
+    };
+
+    if (mainNavToggleElement) {
+      mainNavToggleElement.addEventListener('click', toggleMenu);
+    }
+
+    if (navItemsCollection.length > 0) {
+      navItemsCollection.forEach((item) => {
+        const link = item.querySelector('a');
+        link.addEventListener('click', toggleMenu);
+      })
+    }
+  }
+})();
+
+'use strict';
+
+(() => {
+  const formElement = document.querySelector('.form-feedback');
+  const Inputmask = window.Inputmask;
+
+  if (formElement) {
+    const telInputElement = formElement.querySelector('#tel');
+    const mask = new Inputmask('9 (999) 999-99-99');
+    if (telInputElement) {
+      mask.mask(telInputElement);
+    }
+  }
+})();
