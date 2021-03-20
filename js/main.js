5b154ce9e25e4cda6dc510c9875db0aa00d80159
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-03-20 22:03:45 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -56,21 +56,12 @@
 'use strict';
 
 (() => {
-  const form = document.querySelector('.form-feedback');
-  const telInputElement = form?.querySelector('#tel');
+  const formElement = document.querySelector('.form-feedback');
+  const Inputmask = window.Inputmask;
 
-  const regex = /^(\+7|7|8)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
-
-  const onInputCheckTel = () => {
-    const value = telInputElement.value;
-    if(!regex.test(value)) {
-      telInputElement.setCustomValidity('Введите 10-значный номер телефона');
-    } else {
-      telInputElement.setCustomValidity('');
-    }
-    telInputElement.reportValidity();
+  if (formElement) {
+    const telInputElement = formElement.querySelector('#tel');
+    const mask = new Inputmask('9 (999) 999-99-99');
+    mask.mask(telInputElement);
   }
-
-  telInputElement?.addEventListener('input', onInputCheckTel);
-
 })();
